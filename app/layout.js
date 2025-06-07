9ef81c95cc82eb51f236d556b4222704c7ce257f
elisha <elishajameel2007@gmail.com>
2025-06-07 21:37:14 +0500

    understood the link component for navbat

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import Navigation from "../components/Navigation";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -32,7 +34,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <h1>Hello Layout</h1>
+        <Navigation/>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>;
+    </>
+  );
 };
 export default Home;
