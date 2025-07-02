ab1beefa48600cc137286a24f8bbd21ed3b24a21
elisha <elishajameel2007@gmail.com>
2025-07-02 13:22:36 +0500

    checking the connection of mysql

--- config/db.js
+++ config/db.js
@@ -6,3 +6,12 @@ export const db = mysql.createPool({
   password: "elishajameelmysql@#$123",
   database: "hospital_db",
 });
+
+try {
+  const connection = await db.getConnection();
+  console.log(connection);
+  connection.release();
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
