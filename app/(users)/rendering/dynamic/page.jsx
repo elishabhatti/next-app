f3e64a01aca4d0ef88e61abfc9171aa2e92c545a
elisha <elishajameel2007@gmail.com>
2025-07-02 17:56:43 +0500

    created dynamic page

---
+++ app/(users)/rendering/dynamic/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { db } from "@/config/db";
+
+const DynamicPage = async () => {
+  const [doctors] = await db.execute("select * from doctors");
+  console.log(doctors);
+
+  return (
+    <div>
+      <h1>Hi Full Stack Developer</h1>
+      <ul>
+        {doctors.map((doctor) => {
+          return <li key={doctor.doctor_id}>{doctor.first_name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default DynamicPage;
