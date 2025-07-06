c247d7bbd2d18aee6f8d7e652d768c83985b3e6d
elisha <elishajameel2007@gmail.com>
2025-07-06 19:01:22 +0500

    added link in 404 page to go back

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="text-center mt-10">
+        <Link href="/rendering/dynamic">Go Back</Link>
       <h1 className="text-3xl font-bold text-red-500">404 Page Not Found</h1>
       <p className="text-gray-600 mt-2">We couldn’t find a doctor with that ID.</p>
     </div>
