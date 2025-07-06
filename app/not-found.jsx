f312b9ab21acf25a37e0812d17758aa0122f52d9
elisha <elishajameel2007@gmail.com>
2025-07-06 18:56:22 +0500

    created a not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold text-red-500">Doctor Not Found</h1>
+      <p className="text-gray-600 mt-2">We couldn’t find a doctor with that ID.</p>
+    </div>
+  );
+}
