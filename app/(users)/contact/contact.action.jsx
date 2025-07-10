f8f033f92628c16f1a47f67dad982077ecc296bf
elisha <elishajameel2007@gmail.com>
2025-07-10 19:54:17 +0500

    insert the data in the table

--- app/(users)/contact/contact.action.jsx
+++ app/(users)/contact/contact.action.jsx
@@ -1,5 +1,12 @@
-"use server"
+"use server";
+import { db } from "@/config/db";
 
 export const contactAction = async (formData) => {
-    
-}
+  const { full_name, email, message } = Object.fromEntries(formData.entries());
+  console.log(full_name, email, message);
+
+  await db.execute(
+    "insert into contact_messages (full_name, email, message) values (?, ?, ?)",
+    [full_name, email, message]
+  );
+};
--- app/(users)/contact/page.jsx
+++ app/(users)/contact/page.jsx
@@ -4,23 +4,27 @@ const Contact = () => {
   return (
     <section className="min-h-screen flex items-center justify-center bg-black px-4">
       <div className="bg-[#0e0e1a] p-8 rounded-lg w-full max-w-md shadow-lg">
-        <h1 className="text-pink-400 text-3xl font-bold text-center mb-6">Get In Touch</h1>
-        
+        <h1 className="text-pink-400 text-3xl font-bold text-center mb-6">
+          Get In Touch
+        </h1>
+
         <form action={contactAction} className="flex flex-col space-y-4">
           <div>
             <label className="text-white text-sm mb-1 block">Full Name</label>
             <input
               type="text"
-              name="name"
+              name="full_name"
               placeholder="Enter your full name"
               className="w-full px-4 py-2 rounded bg-[#1e1e2f] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500"
             />
           </div>
 
           <div>
-            <label className="text-white text-sm mb-1 block">Email Address</label>
+            <label className="text-white text-sm mb-1 block">
+              Email Address
+            </label>
             <input
-            name="email"
+              name="email"
               type="email"
               placeholder="Enter your email address"
               className="w-full px-4 py-2 rounded bg-[#1e1e2f] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500"
