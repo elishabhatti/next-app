bf34851de982e41ae0013169511b53c21c4a3a93
elisha <elishajameel2007@gmail.com>
2025-07-10 19:35:18 +0500

    added names in contact us form

--- app/(users)/contact/page.jsx
+++ app/(users)/contact/page.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
             <label className="text-white text-sm mb-1 block">Full Name</label>
             <input
               type="text"
+              name="name"
               placeholder="Enter your full name"
               className="w-full px-4 py-2 rounded bg-[#1e1e2f] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500"
             />
@@ -17,6 +18,7 @@ const Contact = () => {
           <div>
             <label className="text-white text-sm mb-1 block">Email Address</label>
             <input
+            name="email"
               type="email"
               placeholder="Enter your email address"
               className="w-full px-4 py-2 rounded bg-[#1e1e2f] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500"
@@ -27,6 +29,7 @@ const Contact = () => {
             <label className="text-white text-sm mb-1 block">Message</label>
             <textarea
               rows={5}
+              name="message"
               placeholder="Enter your message..."
               className="w-full px-4 py-2 rounded bg-[#1e1e2f] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none"
             />
