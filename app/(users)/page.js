d7de029cc8bb6cce5525908ecfbb6a7e0a630c3a
elisha <elishajameel2007@gmail.com>
2025-08-14 14:36:27 +0500

    use input from shadcn in next.js

--- app/(users)/page.js
+++ app/(users)/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 const Page = () => {
@@ -13,6 +14,7 @@ const Page = () => {
           Click Me
         </Link>
       </Button>
+      <Input/>
     </>
   );
 };
