86c599d0820ed33bb0610d868750faa64fd4dbb5
elisha <elishajameel2007@gmail.com>
2025-08-22 12:26:14 +0500

    installed and use sheet

--- components/SheetDemo.jsx
+++ components/SheetDemo.jsx
@@ -1,8 +1,27 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const SheetDemo = () => {
   return (
-    <div>SheetDemo</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default SheetDemo
+export default SheetDemo;
