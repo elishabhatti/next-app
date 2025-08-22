692a518d8d48d86a12c2965f2eab2c350b254e30
elisha <elishajameel2007@gmail.com>
2025-08-22 11:12:22 +0500

    use footer in dialog box

--- components/DialogDemo.jsx
+++ components/DialogDemo.jsx
@@ -2,11 +2,13 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const DialogDemo = () => {
   return (
@@ -22,6 +24,12 @@ const DialogDemo = () => {
             account and remove your data from our servers.
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
